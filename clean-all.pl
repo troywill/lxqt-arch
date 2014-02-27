558118184b9d57a029403a98a5d4629dd5d15b6e
Troy Will <troydwill@gmail.com>
2014-02-27 11:48:06 -0800

    new file:   ../liblxqt/PKGBUILD 	new file:   ../libqtxdg/PKGBUILD

---
+++ clean-all.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+my @packages = qw( libqtxdg liblxqt);
+
+foreach my $package (@packages) {
+    chdir ($package) or die "Unable to chdir: $!";
+    chomp(my $pwd = `pwd`);
+    print "[in $pwd]\n";
+    my $command = "rm -rf ${package}* pkg src";
+    print "=> $command\n"; system($command);
+    chdir ("..") or die "Unable to chdir ..: $!";
+}
+
+__END__
+cd libqtxdg
+rm -rf libqtxdg*
+rm -rf pkg
+rm -rf src
+
+cd
