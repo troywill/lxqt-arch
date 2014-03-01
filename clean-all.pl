ae7b1d463b22411c885a37997178414fc09e0903
Troy Will <troydwill@gmail.com>
2014-02-28 16:35:27 -0800

    clean-all.pl: Don't delete .install files

--- clean-all.pl
+++ clean-all.pl
@@ -6,7 +6,7 @@ my @packages = qw( libqtxdg liblxqt liblxqt-mount lxqt-globalkeys lxqt-notificat
 
                    lxqt-panel pcmanfm-qt lxqt-session lxqt-runner lxqt-qtplugin lxqt-appswitcher
                    lxqt-policykit lxqt-openssh-askpass lxqt-power lxqt-powermanagement lximage-qt
-                   lxqt-config lxinput-qt lxqt-config-randr obconf-qt
+                   lxqt-config lxinput-qt lxqt-config-randr obconf-qt compton-conf
 
                    lxqt-common
                 );
@@ -14,7 +14,7 @@ foreach my $package (@packages) {
     chdir ($package) or die "Unable to chdir: $!";
     chomp(my $pwd = `pwd`);
     print "[in $pwd]\n";
-    my $command = "rm -rf ${package}* pkg src";
+    my $command = "rm -rf ${package}/ ${package}*.xz pkg src";
     print "=> $command\n"; system($command);
     chdir ("..") or die "Unable to chdir ..: $!";
 }
